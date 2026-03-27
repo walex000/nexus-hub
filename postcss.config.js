6dbae890e64e1cbaf5111ef6588ce355b49385f8
walex000 <ptwilder17@icloud.com>
2026-03-27 15:41:52 -0500

    Add PostCSS configuration file

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {
+      browsers: 'last 2 versions',
+      stage: 0,
+    },
+    'cssnano': {} 
+  }
+};
